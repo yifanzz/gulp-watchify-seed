bfb953c19e4b2d384320ddd920d930ffc14af7a4
Yifan Zhao <yifanzhao@hotmail.co.uk>
2014-05-25 15:39:28 +0100

    Fixed errornous path after moving web.js

--- server/web.js
+++ server/web.js
@@ -2,6 +2,6 @@ var express = require('express')
 
 var app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/../public'))
 
 app.listen(process.env.PORT || 5000)
